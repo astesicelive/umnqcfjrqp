bae73214c92995a16fa314f8152967a4e8e14db8
leaf <145163317+astesicelive@users.noreply.github.com>
2026-08-21 01:18:23 +0200

    start of mtl button stuff

--- js/lightswitch.js
+++ js/lightswitch.js
@@ -1,23 +1,37 @@
-// DARK MODE
+// DARK MODE, HIDE MTLS
 window.addEventListener("DOMContentLoaded", function() {
-const btn = document.querySelector(".lightswitch");
-$(".lightswitch i").attr("class","ph-moon-fill");
+    const light_btn = document.querySelector(".lightswitch");
+    $(".lightswitch i").attr("class","ph-moon-fill");
 
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-document.body.classList.add("night");
-$(".lightswitch i").attr("class","ph-sun-fill");
-}
+    const currentTheme = localStorage.getItem("theme");
+    if (currentTheme == "dark") {
+        document.body.classList.add("night");
+        $(".lightswitch i").attr("class","ph-sun-fill");
+        $(".lightswitch").attr("title","Light Mode");
+    };
 
-btn.addEventListener("click", function () {
-document.body.classList.toggle("night");
+    light_btn.addEventListener("click", function () {
+        document.body.classList.toggle("night");
 
-let theme = "light";
-$(".lightswitch i").attr("class","ph-moon-fill");
-if (document.body.classList.contains("night")) {
-theme = "dark";
-$(".lightswitch i").attr("class","ph-sun-fill");
-}
-localStorage.setItem("theme", theme);
-});
+        let theme = "Light";
+        $(".lightswitch i").attr("class","ph-moon-fill");
+        if (document.body.classList.contains("night")) {
+            theme = "Dark";
+            $(".lightswitch i").attr("class","ph-sun-fill");
+        };
+        localStorage.setItem("theme", theme.toLowerCase());
+        $(".lightswitch").attr("title", theme + " Mode");
+    });
+
+    const mtl_btn = document.querySelector(".mtl_button");
+    $(".mtl_button i").attr("class","ph-prohibit");
+    
+    const currentMtlDisplay = localStorage.getItem("mtl_display");
+    if (currentMtlDisplay == "none") {
+        $("a.mtl").each(function () {
+            $(this).css("display", "none");
+        });
+        $(".mtl_button i").attr("class","ph-robot");
+        $(".mtl_button").attr("title","Show MTLs");
+    };
 });
--- js/links.js
+++ js/links.js
@@ -10,15 +10,35 @@ const five__navLinks = () => {
       title: 'Supplemental Directory',
       action: 'qrfrqbazdu',
     },
-    'mtl': {
-      icon: 'ph-robot',
+    'mtl_button': {
+      icon: 'ph-prohibit',
       title: 'Show/Hide MTLs',
-      action: 'mtl_action',
+      action: 'mtl',
     },
     'lightswitch': {
       icon: 'ph-moon-fill',
       title: 'Light/Dark Mode',
       action: 'lightswitch',
+    },
+    'era1': {
+      icon: '',
+      title: '! Era Stories',
+      action: 'yqbawklsln',
+    },
+    'era2': {
+      icon: '',
+      title: '!! Era Stories',
+      action: 'zauibutqvo',
+    },
+    'idol': {
+      icon: '',
+      title: 'Idol Stories',
+      action: 'zyrejpmdyx',
+    },
+    'other': {
+      icon: '',
+      title: 'Other Stories',
+      action: 'twocfgmbty',
     },
   };
   return obj;

a05ad1accd8f3bf854f85be31681a6ed6d30e99e
leaf <145163317+astesicelive@users.noreply.github.com>
2026-08-21 00:33:24 +0200

    Update links.js

--- js/links.js
+++ js/links.js
@@ -1,1 +1,27 @@
+const five__navLinks = () => {
+  let obj = {
+    'home': {
+      icon: 'ph-house-fill',
+      title: 'About/Home',
+      action: 'mdmjiqntyz',
+    },
+    'supplemental': {
+      icon: 'ph-list',
+      title: 'Supplemental Directory',
+      action: 'qrfrqbazdu',
+    },
+    'mtl': {
+      icon: 'ph-robot',
+      title: 'Show/Hide MTLs',
+      action: 'mtl_action',
+    },
+    'lightswitch': {
+      icon: 'ph-moon-fill',
+      title: 'Light/Dark Mode',
+      action: 'lightswitch',
+    },
+  };
+  return obj;
+};
+
 

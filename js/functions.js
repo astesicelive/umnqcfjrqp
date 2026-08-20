498c2400fd2882a87ddd1f60bed8a56343edf03b
leaf <145163317+astesicelive@users.noreply.github.com>
2026-08-21 00:33:13 +0200

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -101,32 +101,6 @@ function five__mtlClass() {
 };
 five__mtlClass();
 
-const five__navLinks = () => {
-  let obj = {
-    'home': {
-      icon: 'ph-house-fill',
-      title: 'About/Home',
-      action: 'mdmjiqntyz',
-    },
-    'supplemental': {
-      icon: 'ph-list',
-      title: 'Supplemental Directory',
-      action: 'qrfrqbazdu',
-    },
-    'mtl': {
-      icon: 'ph-robot',
-      title: 'Show/Hide MTLs',
-      action: 'mtl_action',
-    },
-    'lightswitch': {
-      icon: 'ph-moon-fill',
-      title: 'Light/Dark Mode',
-      action: 'lightswitch',
-    },
-  };
-  return obj;
-};
-
 function five__navigationButtons() {
   const buttons = five__navLinks();
   $("header").prepend(

a058678ec59db1e95f6547c743a1dc078c50a510
leaf <145163317+astesicelive@users.noreply.github.com>
2026-08-21 15:15:05 +0200

    nav test

--- js/icons.js
+++ js/icons.js
@@ -108,7 +108,7 @@ function five__navigationButtons() {
     `<div class="links">`,
     `<a href="${buttons.home.action}" title="${buttons.home.title}"><i class="${buttons.home.icon}"></i></a>`,
     `<a href="${buttons.supplemental.action}" title="${buttons.supplemental.title}"><i class="${buttons.supplemental.icon}"></i></a>`,
-    `<a class="${buttons.mtl.action}" title="${buttons.mtl.title}"><i class="${buttons.mtl.icon}"></i></a>`,
+    `<a class="${buttons.mtl_button.action}" title="${buttons.mtl_button.title}"><i class="${buttons.mtl_button.icon}"></i></a>`,
     `<a class="${buttons.lightswitch.action}" title="${buttons.lightswitch.title}"><i class="${buttons.lightswitch.icon}"></i></a>`,
     `</div>`
   );
